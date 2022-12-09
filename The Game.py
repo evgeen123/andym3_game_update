13a5cfe1a1746bed4adbd2e9b1ccaaea6cf820d4
Evgeen1 <jacking988@gmail.com>
2022-12-09 16:55:09 +0300

    dots

--- The Game.py
+++ The Game.py
@@ -2,38 +2,55 @@ print("*" * 59)
 print("*" * 9, " Давай сыграем в игру крестики нолики ", "*" * 10)
 print("*" * 59)
 
-board = list(range(1,10))
+RANGE_OF_FIELD = 6
+board = list(range(1,RANGE_OF_FIELD **2 + 1))
+WIN_ROW = 4
 
-def draw_board(board):  #Создаем тело игрового поля
-   print("█" * 22, "-" * 13, "█" * 22) # █ данный символ реализован через сочетание alt+219
-   for i in range(3):
-      print("█" * 22, "|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|", "█" * 22)
-      print("█" * 22, "-" * 13, "█" * 22)
 
-def take_input(player_token): #создаем ввод для игроков и проверку ввода
-   valid = False
-   while not valid:
-      player_answer = input("Куда поставим  " + player_token)
-      try:
-         player_answer = int(player_answer)
-      except:
-         print("Введен неверный символ")
-         continue
-      if player_answer >= 1 and player_answer <= 9:
-         if(str(board[player_answer-1]) not in "XO"):
+def draw_board(board: list):  #Создаем тело игрового поля
+    print("█" * 22, "-" * (RANGE_OF_FIELD *4) , "█" * 22) # █ данный символ реализован через сочетание alt+219
+    for i in range(RANGE_OF_FIELD):
+        row = ""
+        for j in range(RANGE_OF_FIELD):
+            row += "|"
+            row += str(board[j+i*RANGE_OF_FIELD]).ljust(3)
+        print("█" * 22, row,  "█" * 22)
+        print("█" * 22, "-" * (RANGE_OF_FIELD *4), "█" * 22)
+
+def take_input(player_token: str): #создаем ввод для игроков и проверку ввода
+    valid = False
+    while not valid:
+        player_answer = input(f"Куда поставим  {player_token}" )
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Введен неверный символ")
+            continue
+        if player_answer < 1 or player_answer > RANGE_OF_FIELD ** 2:
+            print(f"Неверный символ. Введите число от 1 до {RANGE_OF_FIELD**2}.")
+            continue
+        if not board[player_answer-1] in ["X", "O"]:
             board[player_answer-1] = player_token
             valid = True
-         else:
-            print("Здесь есть значение")
-      else:
-        print("Неверный символ. Введите число от 1 до 9.")
+        else:
+            print("Здесь уже есть значение")
 
-def check_win(board): #проверка на выигрышь
-   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
-   for each in win_coord:
-       if board[each[0]] == board[each[1]] == board[each[2]]:
-          return board[each[0]]
-   return False
+
+def check_list_of_equals(list1: list):
+    if len(set(list1)) == 1:
+        return True
+    return False
+
+
+def check_win(board : list): #проверка на выигрышь
+    win_coord = []
+    for i in range(RANGE_OF_FIELD):
+        for j in range(RANGE_OF_FIELD - WIN_ROW + 1):
+            if check_list_of_equals(board[i*RANGE_OF_FIELD+j:i*RANGE_OF_FIELD+j+ WIN_ROW]):#горизонталь
+                return True
+            if check_list_of_equals(board[i + j * RANGE_OF_FIELD:i + (j + WIN_ROW)*RANGE_OF_FIELD:RANGE_OF_FIELD]):#вертикаль
+                return True
+    return False
 
 def main(board): #запуск функций
     counter = 0
@@ -45,13 +62,12 @@ def main(board): #запуск функций
         else:
            take_input("O")
         counter += 1
-        if counter > 4:
-           tmp = check_win(board)
-           if tmp:
-              print(tmp, "УРА, Вы выиграли!")
+        if counter > (WIN_ROW + 1):
+           if check_win(board):
+              print("УРА, Вы выиграли!")
               win = True
               break
-        if counter == 9:
+        if counter == RANGE_OF_FIELD**2:
             print("Ничья!")
             break
     draw_board(board)
